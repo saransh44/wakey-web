a8447a673c2a7d7768b4f3edf0c059b1ae2ce471
Shannon Peng <shannonpeng98@gmail.com>
2015-09-06 00:49:50 -0400

    Add alarm sound

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
 var app = angular.module("app", ["firebase"]);
 
+/**
+ * @author Alexander Manzyuk <admsev@gmail.com>
+ * Copyright (c) 2012 Alexander Manzyuk - released under MIT License
+ * https://github.com/admsev/jquery-play-sound
+ * Usage: $.playSound('http://example.org/sound.mp3');
+**/
+
+(function($){
+
+  $.extend({
+    playSound: function(){
+      return $("<embed src='"+arguments[0]+".mp3' hidden='true' autostart='true' loop='false' class='playSound'>" + "<audio autoplay='autoplay' style='display:none;' controls='controls'><source src='"+arguments[0]+".mp3' /><source src='"+arguments[0]+".ogg' /></audio>").appendTo('body');
+    }
+  });
+
+})(jQuery);
+
 $(window).load(function() {
 
 	angular.element($(".container").scope().getCurrentTime());
@@ -8,7 +25,9 @@ $(window).load(function() {
 
 
 	$(".button").click(function() {
-		angular.element($(".container").scope().advance());
+		if (angular.element($(".container").scope().advance()) == 4) {
+			$.playSound("http://soundbible.com/grab.php?id=2061&type=mp3");
+		}
 		if ($("#numHours").val() != "") {
 			angular.element($(".container").scope().setNumHours($("#numHours").val()));
 		}
