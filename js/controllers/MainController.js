33360d120c1a18132a60cf61cd190f643e1ffc37
Shannon Peng <shannonpeng98@gmail.com>
2015-09-06 00:34:26 -0400

    Add more quotes

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -9,7 +9,7 @@ app.controller('MainController', function($scope, $firebaseArray) {
 	$scope.currentTip;
 	$scope.currentTipIndex = 0;
 	$scope.sleepCount = 0;
-	$scope.tips = ["Grab a snack from the fridge.", "Don't give up.", "You can do it.", "You deserve to sleep.", "Thanks for that.", "Never mind."];
+	$scope.tips = ["A word of encouragement during a failure is worth more than an hour of praise after success. -Unknown", "In the middle of every difficulty lies opportunity. -Albert Einstein", "The best revenge is massive success. -Frank Sinatra", "Most of us, swimming against the tides of trouble the world knows nothing about, need only a bit of praise or encouragement – and we will make the goal. -Jerome Fleishman", "Perhaps everything terrible is in its deepest being something helpless that wants help from us. -Rainer Rilke", "Trust yourself. You know more than you think you do. -Ben Spock", "Go confidently in the direction of your dreams. Live the life you have imagined. -Henry Thoreau", "He who refuses to embrace a unique opportunity loses the prize as surely as if he had failed.", "Never let the odds keep you from doing what you know in your heart you were meant to do. -H. Jackson Brown, Jr.", "When you come to the end of your rope, tie a knot and hang on. -Franklin Roosevelt"];
 
 	$scope.advance = function() {
 		$scope.currentTipIndex = 0;
